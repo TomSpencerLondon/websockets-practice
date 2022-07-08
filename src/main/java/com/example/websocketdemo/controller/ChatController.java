e6c76118f949544436dd626e403cc3c08962b9f2
Tom <thomas.spencer@codurance.com>
2022-07-09 00:10:16 +0100

    add websockets

---
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.example.websocketdemo.controller;
+
+import com.example.websocketdemo.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+  @MessageMapping("/chat.sendMessage")
+  @SendTo("/topic/public")
+  public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+    return chatMessage;
+  }
+
+  @MessageMapping("/chat.addUser")
+  @SendTo("/topic/public")
+  public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                             MessageHeaderAccessor headerAccessor) {
+    headerAccessor.setHeader("username", chatMessage.getSender());
+    return chatMessage;
+  }
+}
